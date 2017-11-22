c3a41ae3c3603b4d0a439682c7bee398b32106db
ankitsejwal <ankit@sejw.al>
2017-11-23 05:20:22 +1100

    prac

--- 03_01/start/functions/script.js
+++ 03_01/start/functions/script.js
@@ -1,3 +1,20 @@
-(function(){
-    console.log('what is going on?');
-})();
+// (function(){
+//     console.log('what is going on?');
+// })();
+
+// var park = function(){
+//     console.log('quite well.');
+// }();
+
+var salesman = {
+    name: "vinod",
+    height: 5.10,
+    language: "english"
+}
+
+var run = function(){
+    for (var key in salesman){
+        console.log(key);
+    console.log(salesman[key]);
+    }
+}();
